733dc98eedcb122f26fa7f6d49777d716421d522
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2021-08-17 10:50:42 -0600

    Create Sequence.java
    initial commit

---
+++ Sequence.java
@@ -0,0 +1,37 @@
+import com.phidget22.*;//Add Phidgets Library.
+
+public class Sequence 
+{
+	//Handle Exceptions
+	public static void main(String[] args) throws Exception
+	{
+		//Create
+		DigitalInput redButton = new DigitalInput();
+		DigitalOutput redLED = new DigitalOutput();
+		DigitalInput greenButton = new DigitalInput();
+		DigitalOutput greenLED = new DigitalOutput();
+		
+		//Address
+		redButton.setHubPort(0);
+		redButton.setIsHubPortDevice(true);
+		redLED.setHubPort(1);
+		redLED.setIsHubPortDevice(true);
+		greenButton.setHubPort(5);
+		greenButton.setIsHubPortDevice(true);
+		greenLED.setHubPort(4);
+		greenLED.setIsHubPortDevice(true);
+		
+		//Open 
+        redButton.open(1000);
+        redLED.open(1000);
+        greenButton.open(1000);
+        greenLED.open(1000);
+        
+        //
+		System.out.println("test println");
+	}
+
+}
+//RNG between 0 and 1
+//If 0, flash red, add red to answers array. if 1, flash green, add green to answers array.
+//If correct, game will flash green LED 3 times and the game will continue. If incorrect, game will flash red LED 3 times and software will stop
