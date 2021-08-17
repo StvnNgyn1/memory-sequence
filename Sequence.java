17cf319bc84f840f56f21e23b93de2a699e07e26
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2021-08-17 10:59:17 -0600

    Update Sequence.java
    format fix commit

--- Sequence.java
+++ Sequence.java
@@ -22,16 +22,17 @@ public class Sequence
 		greenLED.setIsHubPortDevice(true);
 		
 		//Open 
-        redButton.open(1000);
-        redLED.open(1000);
-        greenButton.open(1000);
-        greenLED.open(1000);
-        
-        //
+		redButton.open(1000);
+		redLED.open(1000);
+		greenButton.open(1000);
+		greenLED.open(1000);
+		//Process that determines the sequence of the flashing lights.
+		//Randomly generates number between 0 and 1. 0 for red, 1 for green.
 		System.out.println("test println");
 	}
 
 }
 //RNG between 0 and 1
 //If 0, flash red, add red to answers array. if 1, flash green, add green to answers array.
+//While loop
 //If correct, game will flash green LED 3 times and the game will continue. If incorrect, game will flash red LED 3 times and software will stop
