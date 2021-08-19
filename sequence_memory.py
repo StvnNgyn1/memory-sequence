e5ffa947626778e30430243eeb13a443ff3b35cc
StvnNgyn1 <89041795+StvnNgyn1@users.noreply.github.com>
2021-08-19 11:42:41 -0600

    finished(?) python code

--- sequence_memory.py
+++ sequence_memory.py
@@ -7,6 +7,30 @@ import time
 #Required for random number generator
 import random
 
+#Global variable for number of button events. Represents the number of answers recorded.
+numEvents = 0
+
+#Lists for sequenceKey(answer key) and userAnswer
+sequenceKey = []
+userAnswer = []
+
+#Event
+def onRedButton_StateChange(self, state):
+    global turnRedLEDOn
+    global numEvents
+    turnRedLEDOn = state
+    if (state):
+        userAnswer.append(0)
+        numEvents+=1
+#Event
+def onGreenButton_StateChange(self, state):
+    global turnGreenLEDOn
+    global numEvents
+    turnGreenLEDOn = state
+    if (state):
+        userAnswer.append(1)
+        numEvents+=1
+        
 #Create 
 redButton = DigitalInput()
 redLED =  DigitalOutput()
@@ -28,35 +52,26 @@ redButton.openWaitForAttachment(1000)
 redLED.openWaitForAttachment(1000)
 greenButton.openWaitForAttachment(1000)
 greenLED.openWaitForAttachment(1000)
-
-#Lists for sequenceKey(answer key) and userAnswer
-sequenceKey = []
-userAnswer = []
-
-#Event
-def onRedButton_StateChange(self, state):
-    if (state):
-        userAnswer.append(0)
-#Event
-def onGreenButton_StateChange(self, state):
-    if (state):
-        userAnswer.append(1)
     
 #Subscribe to Events 
 redButton.setOnStateChangeHandler(onRedButton_StateChange)
-greenButton.setOnStateChangeHandler(onGreenButton_State_Change)
+greenButton.setOnStateChangeHandler(onGreenButton_StateChange)
+
+#Global variables to turn on/off LEDS
+turnRedLEDOn = False
+turnGreenLEDOn = False
     
-#Process that determines the sequence of the flashing lights.
-#A for loop that assigns an integer of 0 or 1 to the sequenceKey ArrayList until filled to startingNumberOfColours.
-startingNumberOfColours = 3
-for indexToAssign in range(startingNumberOfColours):
-    #Randomly generates number between 0 and 1 to append to the list sequenceKey
-    sequenceKey.append(int(random.random() * 2))
-    
-print("Starting game...")
-continueGame = True
+startingNumberOfColours = 3 #int variable stating the number of colours flashed the game will start with.   
+print("Starting game, look at the LEDS.")
+time.sleep(1)
+continueGame = True #Boolean that determines if while loop below should continue or not.
 #Loop that keeps game going
 while continueGame == True:
+    sequenceKey.clear() #Ensures the sequence is reset for each level
+    #Process that determines the sequence of the flashing lights until the starting number of colours are met.
+    for indexToAssign in range(startingNumberOfColours):
+        sequenceKey.append(int(random.random() * 2))#Randomly generates number between 0 and 1 to append to the list sequenceKey.
+    
     #A for loop that will flash an LED according to the number assigned at the current index position
     for indexToFlash in range(len(sequenceKey)):
         if sequenceKey[indexToFlash] == 0: #0 is assigned to red
@@ -69,20 +84,34 @@ while continueGame == True:
             greenLED.setState(False)
         time.sleep(0.500)
     
-    #Block for events code
+    numEvents = 0#resets number of events pressed before the user answers
+    userAnswer.clear()#//clears list for the users answers before each answer
+    print("Please enter your answer:")
+    while (numEvents < len(sequenceKey)):#While loop that checks if the users answer is complete
+        redLED.setState(turnRedLEDOn)
+        greenLED.setState(turnGreenLEDOn)
+        time.sleep(0.150)
     
-    #A for loop that will take in the users answers and compare it to the sequence answer key. Determines if the user is correct or incorrect. If correct, game moves on. If incorrect, game stops.
+    #A for loop that will take in the users answers and compare it to the sequence answer key. Determines if the user incorrect. if incorrect, the game will stop
     for indexToCompare in range(len(sequenceKey)):
-        if userAnswer[indexToCompare] != sequenceKey[indexToCompare]: #Indicates the user got the sequence incorrect
+        if userAnswer[indexToCompare] != sequenceKey[indexToCompare]:
+            #Indicates the user got the sequence incorrect
             redLED.setState(True)
-            time.sleep(3)
-            redLED.setState(False)
-            continueGame = False
             print("Game over...")
-        else:
-            greenLED.setState(True) #Indicates the user got the sequence correct
-            time.sleep(3)
-            greenLED.setState(False)
-            startingNumberOfColours += 1 #Adds a colour for the next level.
-            print("Starting next level...")
-time.sleep(0.150)
+            time.sleep(2)
+            redLED.setState(False)
+            continueGame = False  
+            break
+        
+    if (continueGame): #If the user was correct on all, the game will continue
+        #Indicates the user got the sequence correct
+        greenLED.setState(True)
+        print("Congrats!")
+        time.sleep(2)
+        greenLED.setState(False)
+        time.sleep(1)
+        print("\n" + "Starting next level...")
+        time.sleep(2)
+        startingNumberOfColours+=1 #Adds a colour for the next level.
+        
+    time.sleep(0.150)
